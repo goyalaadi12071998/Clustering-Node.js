b6b480ee30f15ea77f8f55d2f9d311790a37d7ca
aadesh goyal <goyalaadesh462@gmail.com>
2020-11-12 10:32:16 +0530

    small case

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const express = require('express');
+const cluster = require('cluster');
 
-const app = express();
+console.log(cluster.isMaster);
 
-app.get('/', (req, res) => {
-    res.send('Hi there!');
-});
+if(cluster.isMaster){
+    // Cause running of index.js again
+    //in slave mode 
+    cluster.fork();
 
-app.listen(3000);
+}else{
+
+    const express = require('express');
+
+    const app = express();
+
+    function doWork(duration) {
+        const start = Date.now();
+        while (Date.now() - start < duration) {
+            //console.log('Work');
+        }
+    }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there!');
+    });
+
+    app.listen(3000,function(){
+        console.log('listening');
+    });
+
+}
+

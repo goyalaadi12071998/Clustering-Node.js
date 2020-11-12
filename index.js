d7d9416df80536071ee7735af0dbd2b6b533f44f
aadesh goyal <goyalaadesh462@gmail.com>
2020-11-12 12:52:32 +0530

    final commit

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-const cluster = require('cluster');
+const express = require('express');
 
-console.log(cluster.isMaster);
+const app = express();
 
-if(cluster.isMaster){
-    // Cause running of index.js again
-    //in slave mode 
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-    cluster.fork();
-
-    //There is a limit beacause all the work is done by cpu at all
-    // Cause
-
-}else{
-
-    const express = require('express');
-
-    const app = express();
-
-    function doWork(duration) {
-        const start = Date.now();
-        while (Date.now() - start < duration) {
-            //console.log('Work');
-        }
+function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {
+        //console.log('Work');
     }
-
-    app.get('/', (req, res) => {
-        doWork(5000);
-        res.send('Hi there!');
-    });
-
-    app.get('/fast',function(req, res) {
-        res.send('This is fast');
-    });
-
-    app.listen(3000,function(){
-        console.log('listening');
-    });
-
 }
 
+app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there!');
+});
+
+app.get('/fast',function(req, res) {
+    res.send('This is fast');
+});
+
+app.listen(3000,function(){
+    console.log('listening');
+});

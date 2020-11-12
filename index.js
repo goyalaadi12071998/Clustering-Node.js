9d55b29b0215940646b785c90d9964e7d1f05599
aadesh goyal <goyalaadesh462@gmail.com>
2020-11-12 11:32:19 +0530

    multi-threaded

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ if(cluster.isMaster){
     // Cause running of index.js again
     //in slave mode 
     cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
 
 }else{
 
@@ -23,6 +26,10 @@ if(cluster.isMaster){
     app.get('/', (req, res) => {
         doWork(5000);
         res.send('Hi there!');
+    });
+
+    app.get('/fast',function(req, res) {
+        res.send('This is fast');
     });
 
     app.listen(3000,function(){
